424629ba9befe3ef40c6f9847df58ccb886b14ee
Konst <techno547sch@gmail.com>
2024-12-24 13:49:20 +0300

    Версия проекта от 20.12.2024

--- game.js
+++ game.js
@@ -1,94 +1,107 @@
-var canv = document.getElementById('canv');
-canv.width = 600;
+var canv = document.getElementById('canv'); //Находим элемент с id "canv". Это элемент с тегом canvas в 
+// файле game.html. Canvas - это холст, на котором происходит отрисовка всей графики в игре
+canv.width = 600;  //Задаем размер canvas 600x600 пикселов
 canv.height = 600;
-var ctx = canv.getContext('2d');
+var ctx = canv.getContext('2d'); //получаем контекст холста и сохраняем его в переменную ctx
 
 var fc = 'rgb(0,0,50)';//цвет фона
 var bc = 'rgb(255,255,150)';//цвет шарика
-var state = {
-    x: 300,
+
+var state = { //Объект в котором хранится состояние игры
+    x: 300, // x и y координаты мяча
     y: 300,
-    dx: 1,
-    dy: 2,
-    cx: 300,
-    points: 0,
-    level: 1,
-    gamestart: true,
-    gameover: false,
+    dx: 1, //горизонтальная скорость
+    dy: 2,  //вертикальная скорость
+    cx: 300,  //координаты центра ракетки
+    points: 0, //очки
+    level: 1,  //уровень
+    gamestart: true,  //флаг, показывающий, игра активна или на паузе
+    gameover: false, // флаг, показывающий, что игра закончена
 };
 
-
-
+/*
+Функция showBall рисует или стирает мяч на холсте. Если в качестве входного параметра
+приходит true, то функция рисует мяч, а, если false, то мяч стирается (точнее рисуется такой-же мяч,
+только цветом фона) 
+*/
 function showBall(vis) {
-    if (vis) {
-        ctx.fillStyle = bc;
-        ctx.beginPath();
-        ctx.arc(state.x, state.y, 25, 0, 2 * Math.PI);
-        ctx.fill();
+    if (vis) { // если vis истина, рисуем мяч
+        ctx.fillStyle = bc; //настраиваем заливку на цвет мяча
+        ctx.beginPath(); //для прорисовки сложных фигур нужно начать путь отрисовки
+        ctx.arc(state.x, state.y, 25, 0, 2 * Math.PI); //рисуем круг. первые два параметра - это координаты центра круга
+        //затем, 25 - радиус круга, далее начальный угол и конечный (от 0 до 2 пи - т.е целая окружность)
+        ctx.fill(); //заливаем окружность
     }
-    else {
-        ctx.fillStyle = fc;
+    else { //если vis  ложно, стираем мяч
+        ctx.fillStyle = fc; //аналогично рисуем мяч цветом фона. т.е. стираем его
         ctx.beginPath();
         ctx.arc(state.x, state.y, 26, 0, 2 * Math.PI);
         ctx.fill();
     }
 }
 
+//Данная функция рисует или стирает ракетку
 function показатьРакетку(vis) {
-    if (vis) {
+    if (vis) { //если истина
         ctx.fillStyle = 'rgb(255,0,0)';
-        ctx.fillRect(state.cx - 50, 575, 100, 25);
+        ctx.fillRect(state.cx - 50, 575, 100, 25); //рисуем прямоугольник красного цвета
     }
-    else {
+    else { //иначе рисуем прямоугольник цвета фона (т.е. стираем ракетку)
         ctx.fillStyle = fc;
         ctx.fillRect(state.cx - 51, 574, 102, 27);
     }
 }
 
+//функция осуществляющая движение мяча
 function move() {
-    if (state.gamestart && !state.gameover) {
-        showBall(false);
-        state.x += state.dx;
+    if (state.gamestart && !state.gameover) { //если игра начата, но не закончена
+        showBall(false); //стираем мяч
+        state.x += state.dx; //меняем текущие координаты мяча (прибавляем скорости по оси x и оси y)
         state.y += state.dy;
-        showBall(true);
-        if (state.x > 574 || state.x < 25) state.dx = -state.dx;
-        if (state.y < 25) state.dy = -state.dy;
-        if (state.y >= 549 && state.x >= state.cx - 50 && state.x <= state.cx + 50) {
-            state.dy = -1.0 - Math.random() * getSpeed() * 0.5;
-            state.dx = getDx(state.dy);
-            state.points++;
-            if (state.points % 10 == 0) state.level++;
+        showBall(true); //рисуем мяч по новым координатам
+        if (state.x > 574 || state.x < 25) state.dx = -state.dx; //если мяч коснулся правой или левой границы
+        //меняем знак горизонтальной скорости
+        if (state.y < 25) state.dy = -state.dy; // тоже самое для верхней границы
+        if (state.y >= 549 && state.x >= state.cx - 50 && state.x <= state.cx + 50) {//если мяч коснулся ракетки
+            state.dy = -1.0 - Math.random() * getSpeed() * 0.5;//получаем новую вертикальную скорость с элементом случайности
+            state.dx = getDx(state.dy); //в зависимости от вертикальной составляющей скорости вычисляем горизонтальную
+            //суставляющую скорости с помощью функции getDX. Это нужно, чтобы полная скорость
+            //соответствовала скорости для данного уровня
+            state.points++;  //увеличиваем очки на 1
+            if (state.points % 10 == 0) state.level++; //если количество очков кратно 10, увеличиваем уровень
         }
-        else if (state.y >= 574)
+        else if (state.y >= 574) //если коснулись нижнего края, игра заканчивается
             state.gameover = true;
     }
 }
 
-function getSpeed() {
+function getSpeed() { //функция возвращает скорость в зависимости от уровня. Нужно настроить.
     return 1.0 + 1.0 * state.level
 }
 
-function getDx(dy) {
+function getDx(dy) { //функция возвращает x-составляющую скорости, в зависимости от y-составляющей
     var speed = getSpeed();
-    var dx = Math.sqrt(speed * speed - dy * dy);
-    if (Math.random < 0.5) dx = -dx;
+    var dx = Math.sqrt(speed * speed - dy * dy); //по теореме Пифагора находим x-составляющую скорости
+    if (Math.random() < 0.5) dx = -dx; //случайно выбираем знак горизонтальной скорости (+ или - т.е. вправо или влево)
     return dx;
 }
 
+//по сути игра начинается здесь, поскольку выше мы описывали функции, если их не вызывать, то сами они работать не будут.
+
 ctx.fillStyle = fc;//цвет заливки
-ctx.fillRect(0, 0, 600, 600);//залитый прямоугольник
+ctx.fillRect(0, 0, 600, 600);//залитый прямоугольник - фон
 
-setInterval(move, 10);
-показатьРакетку(true);
+setInterval(move, 10); //вызываем функцию move каждые 10 милисекунд
+показатьРакетку(true); //показывем ракетку
 
-document.addEventListener('keydown', (e) => {
-    if (state.gamestart && !state.gameover) {
-        if (e.key == 'ArrowLeft' && state.cx >= 60) {
-            показатьРакетку(false);
-            state.cx -= 20;
-            показатьРакетку(true);
+document.addEventListener('keydown', (e) => {//добавляем документу слушатель событий. "слушаем" нажатие клавиш
+    if (state.gamestart && !state.gameover) { //если игра начата, но не закончена
+        if (e.key == 'ArrowLeft' && state.cx >= 60) { //если нажата клавиша влево и ракетка не у левого края
+            показатьРакетку(false); //стираем ракетку
+            state.cx -= 20; //уменьшаем ее координаты на 20 пикселов
+            показатьРакетку(true);//рисуем ракетку по новым координатам
         }
-        if (e.key == 'ArrowRight' && state.cx <= 540) {
+        if (e.key == 'ArrowRight' && state.cx <= 540) { //аналогично для клавиши вправо
             показатьРакетку(false);
             state.cx += 20;
             показатьРакетку(true);
--- script.js
+++ script.js
@@ -30,62 +30,70 @@ function sum(a, b) {
     return c;
 }
 
-console.log('Привет!');
+/************************************************************************************
+ * код выше объяснялся на отдном из предыдущих уроках
+ */
+
+console.log('Привет!'); // данная функция служит для отображения значений в консоли
 console.log(sum(10, 15));
 console.log(sum(7, 20));
 
-function showModal() {
-    var win = document.getElementById('modal');
-    win.style.display = 'block';
+function showModal() { //фунция, которая делает модальное окно видимым
+    var win = document.getElementById('modal'); //находим элемент на странице с id modal
+    win.style.display = 'block';// меняем его свойство стиля с none на blocl
 }
 
-var bClose = document.getElementById('button_close');
-bClose.addEventListener('click', closeModal);
+var bClose = document.getElementById('button_close');//находим элемент с id button_close - кнопку закрытия модального окна
+bClose.addEventListener('click', closeModal);//добавляем слушатель событий на событие "клавиша мыши". При нажатии
+//запускаем фукнцию closeModal
 
-function closeModal() {
-    var win = document.getElementById('modal');
-    win.style.display = 'none';
+function closeModal() {//функция, которая делает модальное окно невидимым
+    var win = document.getElementById('modal');//находим модальное окно
+    win.style.display = 'none';//ставим ему свойство отображения none
 }
-
+/*
+Данная функия создает новую карточку товара. На вход поступают следующие данные:
+путь к картинке, название товара, описание товара, цена и его id в базе данных
+*/
 function createCard(img, title, desc, price, id = 0) {
-    var cardWrapper = document.createElement('div');
+    var cardWrapper = document.createElement('div');//создаем div-элемент (блок) с классом card
     cardWrapper.className = 'card';
 
-    var imgW = document.createElement('div');
+    var imgW = document.createElement('div');//создаем div-элемент(блок) с классом card_image_wrapper
     imgW.className = 'card_image_wrapper';
-    var imgE = document.createElement('img');
+    var imgE = document.createElement('img');//создаем img-элемента с классом card_image
     imgE.className = 'card_image';
-    imgE.src = img;
-    imgW.append(imgE);
-    cardWrapper.append(imgW);
+    imgE.src = img; //в свойство, отвечающее за источник изображения, копируем значение пути к картинке
+    imgW.append(imgE); //делаем картинку дочерним компонентом для блока класса card_image_wrapper
+    cardWrapper.append(imgW);//делаем блок класса card_image_wrapper дочерним компонентом блока card
 
-    var cnw = document.createElement('div');
+    var cnw = document.createElement('div');//создаем div-элемент(блок) с классом card_name_wrapper
     cnw.className = 'card_name_wrapper';
-    cnw.innerHTML = '&laquo;' + title + '&raquo;';
-    cardWrapper.append(cnw);
+    cnw.innerHTML = '&laquo;' + title + '&raquo;';//в содержимое блока копируем название товара
+    cardWrapper.append(cnw);//делаем блок класса card_name_wrapper дочерним компонентом блока card
 
-    var cdw = document.createElement('div');
+    var cdw = document.createElement('div');//создаем div-элемент(блок) с классом card_description_wrapper
     cdw.className = 'card_description_wrapper';
-    cdw.innerHTML = desc;
-    cardWrapper.append(cdw);
+    cdw.innerHTML = desc;//в содержимое блока копируем описание товара
+    cardWrapper.append(cdw);//делаем блок класса card_description_wrapper дочерним компонентом блока card
 
-    var cpw = document.createElement('div');
+    var cpw = document.createElement('div');//создаем div-элемент(блок) с классом card_price_wrapper
     cpw.className = 'card_price_wrapper';
-    cpw.innerHTML = '<b>Цена: </b>' + price + ' р.';
-    cardWrapper.append(cpw);
+    cpw.innerHTML = '<b>Цена: </b>' + price + ' р.';//в содержимое блока копируем цену товара
+    cardWrapper.append(cpw);//делаем блок класса card_price_wrapper дочерним компонентом блока card
 
-    var cb = document.createElement('div');
+    var cb = document.createElement('div');//создаем div-элемент(блок) с классом card_button
     cb.className = 'card_button';
-    cb.innerHTML = 'Купить';
-    cb.id = 'card_button_' + id;
-    cardWrapper.append(cb);
+    cb.innerHTML = 'Купить';//в содержимое блока копируем текст "Купить"
+    cb.id = 'card_button_' + id;//назначаем id для данного блока, в состав которого входит id товара из базы данных
+    cardWrapper.append(cb);//делаем блок класса card_button дочерним компонентом блока card
 
-    var mn = document.getElementById('main');
-    mn.append(cardWrapper);
+    var mn = document.getElementById('main');//находим элемент (блок) с классом main
+    mn.append(cardWrapper);//делаем блок класса card дочерним компонентом блока main
 }
 
-createCard('./img/tv.png', 'Ноутбук', 'Какое-то описание', 20000);
-createCard('./img/tv.png', 'Монитор', 'Еще какое-то описание', 15000);
+createCard('./img/tv.png', 'Ноутбук', 'Какое-то описание', 20000); //создаем карточку товара
+createCard('./img/tv.png', 'Монитор', 'Еще какое-то описание', 15000);//создаем еще одну карточку
 
-for (var i = 0; i < 100; i++)
+for (var i = 0; i < 100; i++) // создаем 100 одинаковых карточек товара
     createCard('./img/tv.png', 'Монитор', 'Еще какое-то описание', 15000, i);
