e277210ac04dd012de2f44437ac0b5b0ac6f420f
Konst <mail@mail.ru>
2025-01-09 16:32:45 +0300

    Добавлен index.php

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
-<?php
-echo "<h1>Hello World!!!</h1><br>";
-$a = 5;
-$b = 12;
-$c = $a + $b;
-echo "Сумма a и b равна " . $c . "<br>";
-for ($i = 1; $i < 11; $i++)
-    echo "<h1>Строка № " . $i . "</h1>";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    echo "<h1>Hello World!!!</h1><br>";
+    $a = 5;
+    $b = 12;
+    $c = $a + $b;
+    echo "Сумма a и b равна " . $c . "<br>";
+    for ($i = 1; $i < 11; $i++)
+        echo "<h1>Строка № " . $i . "</h1>";
+    ?>
+</body>
+
+</html>
