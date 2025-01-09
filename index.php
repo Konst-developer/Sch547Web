6bb3e7524fbf323167ca05b01ea8730c99e9992b
Konst <mail@mail.ru>
2025-01-09 16:28:18 +0300

    Добавлен index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+echo "<h1>Hello World!!!</h1><br>";
+$a = 5;
+$b = 12;
+$c = $a + $b;
+echo "Сумма a и b равна " . $c . "<br>";
+for ($i = 1; $i < 11; $i++)
+    echo "<h1>Строка № " . $i . "</h1>";
