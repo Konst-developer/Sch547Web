2a60600d8c2c02e29f919a8193ddfe056097cbab
Konst <mail@mail.ru>
2025-01-31 16:41:02 +0300

    Создан класс Request

--- App/engine/Autoload.php
+++ App/engine/Autoload.php
@@ -7,7 +7,7 @@ class Autoload
     public function loadClass($className)
     {
         $fileName = str_replace('\\', DS, $className);
-        $fileName = str_replace('app' . DS, ROOT . DS, $fileName) . 'php';
+        $fileName = str_replace('app' . DS, ROOT . DS, $fileName) . '.php';
 
         if (file_exists($fileName))
             include $fileName;
--- App/public/index.php
+++ App/public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use app\engine\Autoload;
+use app\engine\Request;
 
 error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 
@@ -8,3 +9,8 @@ include "../config/config.php";
 include "../engine/Autoload.php";
 
 spl_autoload_register([new Autoload(), 'loadClass']);
+
+$request = new Request();
+
+$controllerName = $request->getControllerName() ?: 'shop';
+$actionName = $request->getActionName() ?: 'index';
