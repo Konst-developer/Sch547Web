0310d0f0b579e6db7deb1e41e5228e116c1d6890
Konst <mail@mail.ru>
2025-02-14 16:22:16 +0300

    Добавлен engin/Db.php

--- App/engine/Db.php
+++ App/engine/Db.php
@@ -15,4 +15,65 @@ class Db
         'charset' => 'utf8'
     ];
     use TSingleton;
+    private $connection = null;
+
+    private function PrepareDsnString()
+    {
+        return sprintf(
+            "%s:host=%s;dbname=%s;charset=%s",
+            $this->config['driver'],
+            $this->config['host'],
+            $this->config['database'],
+            $this->config['charset']
+        );
+    }
+    private function getConnection()
+    {
+        if (is_null($this->connection)) {
+            $this->connection = new \PDO(
+                $this->PrepareDsnString(),
+                $this->config['login'],
+                $this->config['password'],
+            );
+            $this->connection->setAttribute(
+                \PDO::ATTR_DEFAULT_FETCH_MODE,
+                \PDO::FETCH_ASSOC
+            );
+        }
+        return $this->connection;
+    }
+
+    private function query($sql, $params)
+    {
+        $STH = $this->getConnection()->prepare($sql);
+        $STH->execute($params);
+        return $STH;
+    }
+
+    public function queryOne($sql, $params = [])
+    {
+        return $this->query($sql, $params)->fetch();
+    }
+
+    public function queryWhereAssoc($sql, $params = [])
+    {
+        $STH = $this->query($sql, $params);
+        $STH->setFetchMode(\PDO::FETCH_ASSOC);
+        return $STH->fetchAll();
+    }
+
+    public function queryAll($sql, $params = [])
+    {
+        return $this->query($sql, $params)->fetchAll();
+    }
+
+    public function execute($sql, $params = [])
+    {
+        return $this->query($sql, $params)->rowCount();
+    }
+
+    public function lastInsertId()
+    {
+        return $this->connection->lastInsertId();
+    }
 }
