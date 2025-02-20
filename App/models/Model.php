b07415f25eb1acb33ce3006ab290a97f2382e46d
Konst <mail@mail.ru>
2025-02-20 15:40:39 +0300

    Мелкие правки

---
+++ App/models/Model.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace app\models;
+
+use app\interfaces\IModel;
+
+abstract class Model implements IModel
+{
+    protected $props = [];
+    public function __set($name, $value)
+    {
+        if (array_key_exists($name, $this->props)) {
+            $this->name = $value;
+            $this->props[$name] = true;
+        }
+    }
+
+    public function __get($name)
+    {
+        return $this->$name;
+    }
+}
