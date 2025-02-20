22cdd58ab5977b4fcef561691a40c54fa8073c73
Konst <mail@mail.ru>
2025-02-20 16:34:12 +0300

    DBModel

--- App/models/DBModel.php
+++ App/models/DBModel.php
@@ -12,6 +12,13 @@ abstract class DBModel extends Model
     {
         $tableName = static::getTableName();
         $sql = "DELETE FROM {$tableName}";
-        return DB::getInstance()->query($sql, []);
+        return Db::getInstance()->query($sql, []);
+    }
+
+    public static function getWhereAssoc($name, $sign, $value)
+    {
+        $tableName = static::getTableName();
+        $sql = "SELECT * FROM {$tableName} WHERE {$name}{$sign}:{$name}";
+        return Db::getInstance()->queryWhereAssoc($sql, [$name => $value]);
     }
 }
