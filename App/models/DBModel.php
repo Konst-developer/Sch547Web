6caa1ac122aede70420dcfba625a8ce088a6b80c
Konst-Dev <developerkae@outlook.com>
2025-02-21 16:31:01 +0300

    Добавлены методы в DBModel

--- App/models/DBModel.php
+++ App/models/DBModel.php
@@ -21,4 +21,34 @@ abstract class DBModel extends Model
         $sql = "SELECT * FROM {$tableName} WHERE {$name}{$sign}:{$name}";
         return Db::getInstance()->queryWhereAssoc($sql, [$name => $value]);
     }
+    public static function getOne($id)
+    {
+        $tableName = static::getTableName();
+        $sql = "SELECT * FROM {$tableName} WHERE id=:id";
+        return Db::getInstance()->queryOne($sql, ['id' => $id]);
+    }
+
+    public static function getAll()
+    {
+        $tableName = static::getTableName();
+        $sql = "SELECT * FROM {$tableName}";
+        return Db::getInstance()->queryAll($sql);
+    }
+
+    protected function insert()
+    {
+        $columns = [];
+        $params = [];
+        $tableName = static::getTableName();
+        foreach ($this->props as $key => $value) {
+            $params[':' . $key] = $this->$key;
+            $columns[] = $key;
+        }
+        $columns = implode(',', $columns);
+        $values = implode(',', array_keys($params));
+        $sql = "INSERT INTO {$tableName} ({$columns}) VALUES ({$values})";
+        Db::getInstance()->execute($sql, $params);
+        $this->id = Db::getInstance()->lastInsertId();
+        return $this;
+    }
 }
