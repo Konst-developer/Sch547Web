172628034ad89f4e309ce7665eff3ec465b3c024
Konst <mail@mail.ru>
2025-02-28 16:03:20 +0300

    2025-02-28

--- App/controllers/ShopController.php
+++ App/controllers/ShopController.php
@@ -2,10 +2,19 @@
 
 namespace app\controllers;
 
+use app\models\User;
+
 class ShopController extends Controller
 {
     public function actionIndex()
     {
         echo $this->render('index', ['name' => 'Konst']);
+    }
+
+    public function actionInsertUser()
+    {
+        $user = new User('Администратор', 'admin@mail.ru', '1234', 'admin');
+        $user->save();
+        echo "Пользователь {$user->name} сохранен!";
     }
 }
